9382740b9cd2973429bfc1e5072fb8f7fa5c440a
manishsingh8 <manishsingh04031998@gmail.com>
2023-06-05 17:48:24 +0530

    Plans_Section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Header from './components/Header';
 import Baner from './components/Baner';
 import Expert from './components/Expert';
+import NewsLater from './components/NewsLater';
+import Plans from './components/Plans';
 function App() {
   return (
     <>
       <Header />
       <Baner />
       <Expert />
+      <NewsLater />
+      <Plans />
     </>
   );
 }
--- src/components/Expert.js
+++ src/components/Expert.js
@@ -5,15 +5,19 @@ const Expert = () => {
     <>
       <div className="max-w-[1240px] p-2 mx-auto my-10  md:grid grid-cols-2">
         <div className="border border-[blue] h-[200px] col-span-1 md:w-[80%] text-center">
-         {/* <img src="laptop.jpg" alt="" className="inline" /> */}
-
+          {/* <img src="laptop.jpg" alt="" className="inline" /> */}
         </div>
         <div className=" h-[200px] col-span-1 flex flex-col justify-center">
-            <h1 className="text-[#00df9a] font-bold my-2"> Learn From Experts</h1>
-            <p className="my-2 text-justify">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium odit porro ex consequatur. Odit tempore quo asperiores corrupti. Perspiciatis asperiores ipsa consectetur alias sint vitae expedita non incidunt quaerat nihil!
-            </p>
-            <button className=" w-[25%] bg-black text-white p-2 mt-2 rounded">Get Started</button>
+          <h1 className="text-[#00df9a] font-bold my-2"> Learn From Experts</h1>
+          <p className="my-2 text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+            odit porro ex consequatur. Odit tempore quo asperiores corrupti.
+            Perspiciatis asperiores ipsa consectetur alias sint vitae expedita
+            non incidunt quaerat nihil!
+          </p>
+          <button className=" w-[25%] bg-black text-white p-2 mt-2 rounded">
+            Get Started
+          </button>
         </div>
       </div>
     </>
